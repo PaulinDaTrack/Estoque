fb8dd4b8b8f19226ebab380f6c1a9f6963b24661
paauloDev <paulo_victor@ufms.com.br>
2025-02-07 14:55:21 -0400

    PORTA 3306

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ def conectar_banco():
         host=os.getenv('DB_HOST'),
         user=os.getenv('DB_USER'),
         password=os.getenv('DB_PASSWORD'),
-        database=os.getenv('DB_NAME')
+        database=os.getenv('DB_NAME'),
+        port=os.getenv('DB_PORT', '3306')
     )
 
 def criar_tabela_usuarios():
